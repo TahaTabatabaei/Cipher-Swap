6cac4d2ad34e46b37f925a10be389f82494b2bfe
Amir1848 <amir.hesari1848@gmail.com>
2021-11-22 23:03:56 +0330

    MainProcess with Decoder

--- MainProcess.c
+++ MainProcess.c
@@ -21,6 +21,43 @@ int main(){
     }else if( first_ID == 0 && second_ID > 0 ){
 
     }else if( first_ID > 0 && second_ID > 0 ){
+
+
+        //Reading from file
+        char *filename = "test.txt";
+        FILE *fp = fopen(filename, "r");
+
+        if (fp == NULL)
+        {
+            printf("Error: could not open file %s", filename);
+            return 1;
+        }
+
+        const int Max_File_Size = 10000;
+        char str[Max_File_Size];
+        int counter = 0;
+        char ch;
+        while ((ch = fgetc(fp)) != EOF && counter < Max_File_Size ){
+            str[counter] = ch;
+            counter++;
+        }
+        fclose(fp);
+        //end - Reading from file
+
+        int partCounter = 0;
+        int partDivider[2]; 
+        for(int i = 0; i < counter ; i++){
+            if(str[i] == '#' && str[i + 1] == '#' && str[i + 2] == '#'){
+                partDivider[partCounter] = i - 1;
+                partCounter++;
+                i = i + 3;
+            }
+        }   
+
+        //part 1;
+        char str_Part1[partDivider[0] + 1];
+        memcpy(str_Part1, &str[0], (partDivider[0]+ 1));
+        str_Part1[partDivider[0] + 1] = '\0';
  
     }
 
