057edc82c193b9888765bc5d97ee4a223f3082dc
Amir1848 <amir.hesari1848@gmail.com>
2021-11-22 23:08:12 +0330

    placer.cs

---
+++ placer.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <string.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+
+int main(){
+    char mainDecoderFifo[] = "Main_Placer";
+    int fd = open(mainDecoderFifo, O_RDONLY);
+    char phrase[10000];
+    int i = 0;
+    read(fd, &phrase[i], sizeof(char));
+    while(phrase[i] != '\0'){
+        i++;
+        read(fd, &phrase[i], sizeof(char));
+    }
+    close(fd);
+
+    // printf("here is placer: %s",phrase);
+
+    char finderDecoderFifo[] = "Finder_Placer";
+    int fifo = open(finderDecoderFifo, O_RDONLY);
+
+    char str[10000];
+    i = 0;
+    read(fd, &str[i], sizeof(char));
+    while(str[i] != '\0'){
+        i++;
+        read(fd, &str[i], sizeof(char));
+    }
+    close(fd);
+
+    printf("here id placer: %s",str);
+
+    char finalMessage[10000];
+    int finalMessageCounter = 0;
+
+    char * token = strtok(str, "=");
+    for(int k = 0; k < strlen(phrase); k++){
+        if(phrase[k] == '$'){
+            for(int q = 0; q < strlen(token) ; q++){
+                finalMessage[finalMessageCounter] = token[q];
+                finalMessageCounter++;
+            }
+            token = strtok(NULL, "=");
+        } else{
+            finalMessage[finalMessageCounter] = phrase[k];
+            finalMessageCounter++; 
+        }
+    }
+
+    // printf("\nfinal : %s\n",finalMessage);
+    char placerMainFifo[] = "Placer_Main";
+    mkfifo("Placer_Main",0666);
+    int placerMain_fd = open(placerMainFifo,O_WRONLY);
+    write(placerMain_fd,finalMessage, strlen(finalMessage));
+    close(placerMain_fd);
+    return 0;
+}
