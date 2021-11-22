09bc8cf716c779d49d1a617642dd2e84b54a2fd8
Amir1848 <amir.hesari1848@gmail.com>
2021-11-22 22:58:52 +0330

    forking

--- MainProcess.c
+++ MainProcess.c
@@ -1,3 +1,28 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <string.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+
 int main(){
     
+    int first_ID = fork();
+    int second_ID = fork();
+
+
+ if( first_ID == 0 && second_ID == 0 ){
+
+    }else if( first_ID > 0 && second_ID == 0 ){
+    
+    }else if( first_ID == 0 && second_ID > 0 ){
+
+    }else if( first_ID > 0 && second_ID > 0 ){
+ 
+    }
+
+    return 0;
 }
--- decoder.c
+++ decoder.c
@@ -1,3 +1,13 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <string.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+
 int main(){
-    
+
 }
